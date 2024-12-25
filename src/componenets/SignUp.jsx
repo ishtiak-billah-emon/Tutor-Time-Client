582b22a213fcfa058e8d12a8af47fda7d74491b2
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2024-12-25 13:15:21 +0600

    feat(signup): Implement signup page with database and firebase

---
+++ src/componenets/SignUp.jsx
@@ -0,0 +1,123 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
+
+
+const SignUp = () => {
+  const { createUser, setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const image = form.image.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    // firebase
+
+    createUser(email, password)
+      .then((result) => {
+        setUser(result.user);
+        // Signed up
+        // database connection
+         const createdAt = result?.user?.metadata?.creationTime;
+         const user = { name, image, email, createdAt };
+        //  Update profile
+
+        fetch("http://localhost:3000/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(user),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // console.log("user created to db: ", data);
+            if (data.insertedId) {
+              Swal.fire("Successfully signed up!");
+              navigate("/");
+            }
+          });
+         
+         
+        
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log( errorCode, errorMessage);
+        // ..
+      });
+    
+  };
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <form className="card-body" onSubmit={handleSignup}>
+          {/* Name */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              name="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          {/* photoURL */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your photo url"
+              name="image"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          {/* email */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              name="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          {/* pass */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
