274d531c3a0148d7075af38a8fa69ef781c84cfb
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2024-12-25 13:39:42 +0600

    feat(firebase): Implement firebase setup

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqGFVXDP1Up-KUO-h-omSeoUZ-schdaJ4",
+  authDomain: "tutor-time-39767.firebaseapp.com",
+  projectId: "tutor-time-39767",
+  storageBucket: "tutor-time-39767.firebasestorage.app",
+  messagingSenderId: "511861606284",
+  appId: "1:511861606284:web:5560a925ed4839528338a9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
