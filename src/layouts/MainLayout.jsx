33df238280c8685f0db0ecce273fd2c00a88ede7
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2024-12-26 21:51:05 +0600

    feat(Footer): Add Footer component in MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "../componenets/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../componenets/Footer";
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar />
-      <div className="max-w-7xl mx-auto"><Outlet /></div>
-      
+      <Outlet/>     
       {/* Footer */}
+      <Footer/>
     </div>
   );
 };
