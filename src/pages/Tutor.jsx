a047013df68d39db30ec74bd27b152aabd6d99ee
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2024-12-26 21:53:24 +0600

    style: Card design change

--- src/pages/Tutor.jsx
+++ src/pages/Tutor.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import star from "../assets/Images/ratingStar.png";
 
 const Tutor = ({ tutor }) => {
   // console.log("Tutor:", tutor);
-  const {_id, name, image, email, language, description, rating } = tutor;
+  const { _id, name, photo, email, language, description, rating } = tutor;
+
   const navigate = useNavigate();
   return (
-    <div className="bg-slate-200 flex gap-3 rounded-xl mb-3">
-      <div className="p-2 border border-black rounded-xl">
-        <img className="w-48 " src={image} alt="Tutor Image"/>
+    <div className="bg-white border-2 border-black flex gap-3 md:gap-12 rounded-xl mb-3 p-6 ">
+      {/* photo */}
+      <div className="rounded-xl flex-1">
+        <img className="w-auto h-auto md:h-60" src={photo} alt="Tutor Image" />
       </div>
-      <div>
+
+      {/* DESCRIPTION */}
+      <div className="flex-[2] space-y-3">
         <div className="flex gap-2">
-          <h1>{name}</h1>
-          <h2>Rating: {rating}</h2>
+          <h1 className="font-bold">{name}</h1>
+          <div className="flex gap-1 justify-center items-center">
+            <h2 className="font-semibold"> {rating} </h2>
+            <img className="w-5" src={star} alt="" />
+          </div>
         </div>
-        <h2>Language: {language}</h2>
-        <p>{description}</p>
-        <Link to={`/tutor/${_id}`}  className="btn btn-primary">View Details</Link>
+        <h2 className="font-thin">Language: {language}</h2>
+        <p className="font-thin">{description}</p>
+        <Link to={`/tutor/${_id}`} className="btn border-black md:px-6 bg-[#f66962] text-white">
+          View Details
+        </Link>
       </div>
     </div>
   );
-  };
+};
 
 export default Tutor;
