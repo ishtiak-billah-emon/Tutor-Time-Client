85b2f86278c5dc34e6a16ecdbd9ada40260265c9
Ishtiak Billah Emon <ishtiakemon27@gmail.com>
2024-12-26 21:51:26 +0600

    style: change form design

--- src/pages/AddTutorials.jsx
+++ src/pages/AddTutorials.jsx
@@ -1,16 +1,14 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../provider/AuthProvider';
+import React, { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
 import Swal from "sweetalert2/dist/sweetalert2.js";
 
-
 const AddTutorials = () => {
-
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const name = user?.displayName;
   const email = user?.email;
   // console.log(name, email);
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     const photo = form.photo.value;
@@ -18,9 +16,18 @@ const AddTutorials = () => {
     const price = form.price.value;
     const description = form.description.value;
     const rating = form.rating.value;
-    
-    const newTutorial = { name, email, photo, language, price, description, rating};
-  
+    const review = form.review.value;
+
+    const newTutorial = {
+      name,
+      email,
+      photo,
+      language,
+      price,
+      description,
+      rating,
+      review
+    };
 
     // add to the database
     fetch("http://localhost:3000/tutorials", {
@@ -33,7 +40,7 @@ const AddTutorials = () => {
       .then((res) => res.json())
       .then((data) => {
         // console.log(data);
-        if(data.insertedId){
+        if (data.insertedId) {
           Swal.fire({
             title: "Success!",
             text: `The Tutorial  has been added successfully to the website.`,
@@ -42,24 +49,25 @@ const AddTutorials = () => {
           });
         }
       });
-  
-  }
+  };
   return (
-    <div className="max-w-7xl mx-auto">
-      Add Tutorial
-      <div className="flex gap-4 mb-3">
+    <div className="max-w-7xl mx-auto p-8 md:mt-24 md:py-12  md:rounded-lg md:shadow-md">
+      <h1 className="font-bold text-2xl text-center mb-6 bg-[#e5fae9] h-auto py-3 md:py-6 rounded-lg">
+        Add a new Tutorial!
+      </h1>
+      <div className="flex flex-col md:flex-row gap-4 mb-3">
         <input
           type="text"
           defaultValue={name}
           name="username"
-          className="input input-bordered w-full max-w-xs"
+          className="input input-bordered w-full bg-slate-100 "
           readOnly
         />
         <input
           type="text"
           defaultValue={email}
           name="useremail"
-          className="input input-bordered w-full max-w-xs"
+          className="input input-bordered w-full bg-slate-100  "
           readOnly
         />
       </div>
@@ -73,42 +81,50 @@ const AddTutorials = () => {
             type="text"
             placeholder="Enter the photo url"
             name="photo"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full "
           />
           {/* Language */}
           <input
             type="text"
             placeholder="Enter the language"
             name="language"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full"
           />
           {/* Price */}
           <input
             type="text"
             placeholder="Enter the Price"
             name="price"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full"
           />
           {/* description */}
           <input
             type="text"
             placeholder="Type Description"
             name="description"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full"
           />
           {/* rating */}
           <input
             type="text"
             placeholder="Rating"
-            defaultValue={"0"}
             name="rating"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full"
+          />
+          {/* review */}
+          <input
+            type="text"
+            placeholder="Review"
+            defaultValue={"0"}
+            name="review"
+            className="input input-bordered w-full bg-slate-100 "
+            readOnly
           />
 
           <input
             type="submit"
             value="SUBMIT"
-            className="btn btn-primary w-2/3"
+            className="btn  bg-[#f66962] text-black font-bold "
           />
         </form>
       </div>
